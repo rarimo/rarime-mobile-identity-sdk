ec304019c6b266a0f2a1cff8f3d32cd06a638cb2
ivanlele <ivanlele402@gmail.com>
2024-11-06 16:40:55 +0200

    add Brainpool elliptic curve parameters for various sizes

--- brainpool.go
+++ brainpool.go
@@ -1,9 +1,144 @@
 package identity
 
 import (
+	"crypto/elliptic"
 	"encoding/asn1"
 	"math/big"
 )
+
+var brainpoolP160r1CurveParams = getCurveParamsBrainpoolP160r1()
+var brainpoolP192r1CurveParams = getCurveParamsBrainpoolP192r1()
+var brainpoolP224r1CurveParams = getCurveParamsBrainpoolP224r1()
+var brainpoolP256r1CurveParams = getCurveParamsBrainpoolP256r1()
+var brainpoolP320r1CurveParams = getCurveParamsBrainpoolP320r1()
+var brainpoolP384r1CurveParams = getCurveParamsBrainpoolP384r1()
+var brainpoolP512r1CurveParams = getCurveParamsBrainpoolP512r1()
+
+func getCurveParamsBrainpoolP160r1() *elliptic.CurveParams {
+	p, _ := new(big.Int).SetString("0xe95e4a5f737059dc60dfc7ad95b3d8139515620f", 0)
+	n, _ := new(big.Int).SetString("0xe95e4a5f737059dc60df5991d45029409e60fc09", 0)
+	b, _ := new(big.Int).SetString("0x1e589a8595423412134faa2dbdec95c8d8675e58", 0)
+	gX, _ := new(big.Int).SetString("0xbed5af16ea3f6a4f62938c4631eb5af7bdbcdbc3", 0)
+	gY, _ := new(big.Int).SetString("0x1667cb477a1a8ec338f94741669c976316da6321", 0)
+
+	return &elliptic.CurveParams{
+		P:       p,
+		N:       n,
+		B:       b,
+		Gx:      gX,
+		Gy:      gY,
+		BitSize: 160,
+		Name:    "brainpoolP160r1",
+	}
+}
+
+func getCurveParamsBrainpoolP192r1() *elliptic.CurveParams {
+	p, _ := new(big.Int).SetString("0xc302f41d932a36cda7a3463093d18db78fce476de1a86297", 0)
+	n, _ := new(big.Int).SetString("0xc302f41d932a36cda7a3462f9e9e916b5be8f1029ac4acc1", 0)
+	b, _ := new(big.Int).SetString("0x469a28ef7c28cca3dc721d044f4496bcca7ef4146fbf25c9", 0)
+	gX, _ := new(big.Int).SetString("0xc0a0647eaab6a48753b033c56cb0f0900a2f5c4853375fd6", 0)
+	gY, _ := new(big.Int).SetString("0x14b690866abd5bb88b5f4828c1490002e6773fa2fa299b8f", 0)
+
+	return &elliptic.CurveParams{
+		P:       p,
+		N:       n,
+		B:       b,
+		Gx:      gX,
+		Gy:      gY,
+		BitSize: 192,
+		Name:    "brainpoolP192r1",
+	}
+}
+
+func getCurveParamsBrainpoolP224r1() *elliptic.CurveParams {
+	p, _ := new(big.Int).SetString("0xd7c134aa264366862a18302575d1d787b09f075797da89f57ec8c0ff", 0)
+	n, _ := new(big.Int).SetString("0xd7c134aa264366862a18302575d0fb98d116bc4b6ddebca3a5a7939f", 0)
+	b, _ := new(big.Int).SetString("0x2580f63ccfe44138870713b1a92369e33e2135d266dbb372386c400b", 0)
+	gX, _ := new(big.Int).SetString("0xd9029ad2c7e5cf4340823b2a87dc68c9e4ce3174c1e6efdee12c07d", 0)
+	gY, _ := new(big.Int).SetString("0x58aa56f772c0726f24c6b89e4ecdac24354b9e99caa3f6d3761402cd", 0)
+
+	return &elliptic.CurveParams{
+		P:       p,
+		N:       n,
+		B:       b,
+		Gx:      gX,
+		Gy:      gY,
+		BitSize: 224,
+		Name:    "brainpoolP224r1",
+	}
+}
+
+func getCurveParamsBrainpoolP256r1() *elliptic.CurveParams {
+	p, _ := new(big.Int).SetString("0xa9fb57dba1eea9bc3e660a909d838d726e3bf623d52620282013481d1f6e5377", 0)
+	n, _ := new(big.Int).SetString("0xa9fb57dba1eea9bc3e660a909d838d718c397aa3b561a6f7901e0e82974856a7", 0)
+	b, _ := new(big.Int).SetString("0x26dc5c6ce94a4b44f330b5d9bbd77cbf958416295cf7e1ce6bccdc18ff8c07b6", 0)
+	gX, _ := new(big.Int).SetString("0x8bd2aeb9cb7e57cb2c4b482ffc81b7afb9de27e1e3bd23c23a4453bd9ace3262", 0)
+	gY, _ := new(big.Int).SetString("0x547ef835c3dac4fd97f8461a14611dc9c27745132ded8e545c1d54c72f046997", 0)
+
+	return &elliptic.CurveParams{
+		P:       p,
+		N:       n,
+		B:       b,
+		Gx:      gX,
+		Gy:      gY,
+		BitSize: 256,
+		Name:    "brainpoolP256r1",
+	}
+}
+
+func getCurveParamsBrainpoolP320r1() *elliptic.CurveParams {
+	p, _ := new(big.Int).SetString("0xd35e472036bc4fb7e13c785ed201e065f98fcfa6f6f40def4f92b9ec7893ec28fcd412b1f1b32e27", 0)
+	n, _ := new(big.Int).SetString("0xd35e472036bc4fb7e13c785ed201e065f98fcfa5b68f12a32d482ec7ee8658e98691555b44c59311", 0)
+	b, _ := new(big.Int).SetString("0x520883949dfdbc42d3ad198640688a6fe13f41349554b49acc31dccd884539816f5eb4ac8fb1f1a6", 0)
+	gX, _ := new(big.Int).SetString("0x43bd7e9afb53d8b85289bcc48ee5bfe6f20137d10a087eb6e7871e2a10a599c710af8d0d39e20611", 0)
+	gY, _ := new(big.Int).SetString("0x14fdd05545ec1cc8ab4093247f77275e0743ffed117182eaa9c77877aaac6ac7d35245d1692e8ee1", 0)
+
+	return &elliptic.CurveParams{
+		P:       p,
+		N:       n,
+		B:       b,
+		Gx:      gX,
+		Gy:      gY,
+		BitSize: 320,
+		Name:    "brainpoolP320r1",
+	}
+}
+
+func getCurveParamsBrainpoolP384r1() *elliptic.CurveParams {
+	p, _ := new(big.Int).SetString("0x8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b412b1da197fb71123acd3a729901d1a71874700133107ec53", 0)
+	n, _ := new(big.Int).SetString("0x8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b31f166e6cac0425a7cf3ab6af6b7fc3103b883202e9046565", 0)
+	b, _ := new(big.Int).SetString("0x4a8c7dd22ce28268b39b55416f0447c2fb77de107dcd2a62e880ea53eeb62d57cb4390295dbc9943ab78696fa504c11", 0)
+	gX, _ := new(big.Int).SetString("0x1d1c64f068cf45ffa2a63a81b7c13f6b8847a3e77ef14fe3db7fcafe0cbd10e8e826e03436d646aaef87b2e247d4af1e", 0)
+	gY, _ := new(big.Int).SetString("0x8abe1d7520f9c2a45cb1eb8e95cfd55262b70b29feec5864e19c054ff99129280e4646217791811142820341263c5315", 0)
+
+	return &elliptic.CurveParams{
+		P:       p,
+		N:       n,
+		B:       b,
+		Gx:      gX,
+		Gy:      gY,
+		BitSize: 384,
+		Name:    "brainpoolP384r1",
+	}
+}
+
+func getCurveParamsBrainpoolP512r1() *elliptic.CurveParams {
+	p, _ := new(big.Int).SetString("0xaadd9db8dbe9c48b3fd4e6ae33c9fc07cb308db3b3c9d20ed6639cca703308717d4d9b009bc66842aecda12ae6a380e62881ff2f2d82c68528aa6056583a48f3", 0)
+	n, _ := new(big.Int).SetString("0xaadd9db8dbe9c48b3fd4e6ae33c9fc07cb308db3b3c9d20ed6639cca70330870553e5c414ca92619418661197fac10471db1d381085ddaddb58796829ca90069", 0)
+	b, _ := new(big.Int).SetString("0x3df91610a83441caea9863bc2ded5d5aa8253aa10a2ef1c98b9ac8b57f1117a72bf2c7b9e7c1ac4d77fc94cadc083e67984050b75ebae5dd2809bd638016f723", 0)
+	gX, _ := new(big.Int).SetString("0x81aee4bdd82ed9645a21322e9c4c6a9385ed9f70b5d916c1b43b62eef4d0098eff3b1f78e2d0d48d50d1687b93b97d5f7c6d5047406a5e688b352209bcb9f822", 0)
+	gY, _ := new(big.Int).SetString("0x7dde385d566332ecc0eabfa9cf7822fdf209f70024a57b1aa000c55b881f8111b2dcde494a5f485e5bca4bd88a2763aed1ca2b2fa8f0540678cd1e0f3ad80892", 0)
+
+	return &elliptic.CurveParams{
+		P:       p,
+		N:       n,
+		B:       b,
+		Gx:      gX,
+		Gy:      gY,
+		BitSize: 512,
+		Name:    "brainpoolP512r1",
+	}
+}
 
 type algorithmIdentifier struct {
 	Algorithm  asn1.ObjectIdentifier
@@ -34,132 +169,3 @@ type curve struct {
 	X           asn1.RawContent
 	Y           asn1.RawContent
 }
-
-// import (
-// 	"crypto/elliptic"
-// 	"math/big"
-// 	"sync"
-// )
-
-// var (
-// 	once                   sync.Once
-// 	p256t1, p384t1, p512t1 *elliptic.CurveParams
-// 	p256r1, p384r1, p512r1 *rcurve
-// )
-
-// func initAll() {
-// 	initP256t1()
-// 	initP384t1()
-// 	initP512t1()
-// 	initP256r1()
-// 	initP384r1()
-// 	initP512r1()
-// }
-
-// func initP256t1() {
-// 	p256t1 = &elliptic.CurveParams{Name: "brainpoolP256t1"}
-// 	p256t1.P, _ = new(big.Int).SetString("A9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377", 16)
-// 	p256t1.N, _ = new(big.Int).SetString("A9FB57DBA1EEA9BC3E660A909D838D718C397AA3B561A6F7901E0E82974856A7", 16)
-// 	p256t1.B, _ = new(big.Int).SetString("662C61C430D84EA4FE66A7733D0B76B7BF93EBC4AF2F49256AE58101FEE92B04", 16)
-// 	p256t1.Gx, _ = new(big.Int).SetString("A3E8EB3CC1CFE7B7732213B23A656149AFA142C47AAFBC2B79A191562E1305F4", 16)
-// 	p256t1.Gy, _ = new(big.Int).SetString("2D996C823439C56D7F7B22E14644417E69BCB6DE39D027001DABE8F35B25C9BE", 16)
-// 	p256t1.BitSize = 256
-// }
-
-// func initP256r1() {
-// 	twisted := p256t1
-// 	params := &elliptic.CurveParams{
-// 		Name:    "brainpoolP256r1",
-// 		P:       twisted.P,
-// 		N:       twisted.N,
-// 		BitSize: twisted.BitSize,
-// 	}
-// 	params.Gx, _ = new(big.Int).SetString("8BD2AEB9CB7E57CB2C4B482FFC81B7AFB9DE27E1E3BD23C23A4453BD9ACE3262", 16)
-// 	params.Gy, _ = new(big.Int).SetString("547EF835C3DAC4FD97F8461A14611DC9C27745132DED8E545C1D54C72F046997", 16)
-// 	z, _ := new(big.Int).SetString("3E2D4BD9597B58639AE7AA669CAB9837CF5CF20A2C852D10F655668DFC150EF0", 16)
-// 	p256r1 = newrcurve(twisted, params, z)
-// }
-
-// func initP384t1() {
-// 	p384t1 = &elliptic.CurveParams{Name: "brainpoolP384t1"}
-// 	p384t1.P, _ = new(big.Int).SetString("8CB91E82A3386D280F5D6F7E50E641DF152F7109ED5456B412B1DA197FB71123ACD3A729901D1A71874700133107EC53", 16)
-// 	p384t1.N, _ = new(big.Int).SetString("8CB91E82A3386D280F5D6F7E50E641DF152F7109ED5456B31F166E6CAC0425A7CF3AB6AF6B7FC3103B883202E9046565", 16)
-// 	p384t1.B, _ = new(big.Int).SetString("7F519EADA7BDA81BD826DBA647910F8C4B9346ED8CCDC64E4B1ABD11756DCE1D2074AA263B88805CED70355A33B471EE", 16)
-// 	p384t1.Gx, _ = new(big.Int).SetString("18DE98B02DB9A306F2AFCD7235F72A819B80AB12EBD653172476FECD462AABFFC4FF191B946A5F54D8D0AA2F418808CC", 16)
-// 	p384t1.Gy, _ = new(big.Int).SetString("25AB056962D30651A114AFD2755AD336747F93475B7A1FCA3B88F2B6A208CCFE469408584DC2B2912675BF5B9E582928", 16)
-// 	p384t1.BitSize = 384
-// }
-
-// func initP384r1() {
-// 	twisted := p384t1
-// 	params := &elliptic.CurveParams{
-// 		Name:    "brainpoolP384r1",
-// 		P:       twisted.P,
-// 		N:       twisted.N,
-// 		BitSize: twisted.BitSize,
-// 	}
-// 	params.Gx, _ = new(big.Int).SetString("1D1C64F068CF45FFA2A63A81B7C13F6B8847A3E77EF14FE3DB7FCAFE0CBD10E8E826E03436D646AAEF87B2E247D4AF1E", 16)
-// 	params.Gy, _ = new(big.Int).SetString("8ABE1D7520F9C2A45CB1EB8E95CFD55262B70B29FEEC5864E19C054FF99129280E4646217791811142820341263C5315", 16)
-// 	z, _ := new(big.Int).SetString("41DFE8DD399331F7166A66076734A89CD0D2BCDB7D068E44E1F378F41ECBAE97D2D63DBC87BCCDDCCC5DA39E8589291C", 16)
-// 	p384r1 = newrcurve(twisted, params, z)
-// }
-
-// func initP512t1() {
-// 	p512t1 = &elliptic.CurveParams{Name: "brainpoolP512t1"}
-// 	p512t1.P, _ = new(big.Int).SetString("AADD9DB8DBE9C48B3FD4E6AE33C9FC07CB308DB3B3C9D20ED6639CCA703308717D4D9B009BC66842AECDA12AE6A380E62881FF2F2D82C68528AA6056583A48F3", 16)
-// 	p512t1.N, _ = new(big.Int).SetString("AADD9DB8DBE9C48B3FD4E6AE33C9FC07CB308DB3B3C9D20ED6639CCA70330870553E5C414CA92619418661197FAC10471DB1D381085DDADDB58796829CA90069", 16)
-// 	p512t1.B, _ = new(big.Int).SetString("7CBBBCF9441CFAB76E1890E46884EAE321F70C0BCB4981527897504BEC3E36A62BCDFA2304976540F6450085F2DAE145C22553B465763689180EA2571867423E", 16)
-// 	p512t1.Gx, _ = new(big.Int).SetString("640ECE5C12788717B9C1BA06CBC2A6FEBA85842458C56DDE9DB1758D39C0313D82BA51735CDB3EA499AA77A7D6943A64F7A3F25FE26F06B51BAA2696FA9035DA", 16)
-// 	p512t1.Gy, _ = new(big.Int).SetString("5B534BD595F5AF0FA2C892376C84ACE1BB4E3019B71634C01131159CAE03CEE9D9932184BEEF216BD71DF2DADF86A627306ECFF96DBB8BACE198B61E00F8B332", 16)
-// 	p512t1.BitSize = 512
-// }
-
-// func initP512r1() {
-// 	twisted := p512t1
-// 	params := &elliptic.CurveParams{
-// 		Name:    "brainpoolP512r1",
-// 		P:       twisted.P,
-// 		N:       twisted.N,
-// 		BitSize: twisted.BitSize,
-// 	}
-// 	params.Gx, _ = new(big.Int).SetString("81AEE4BDD82ED9645A21322E9C4C6A9385ED9F70B5D916C1B43B62EEF4D0098EFF3B1F78E2D0D48D50D1687B93B97D5F7C6D5047406A5E688B352209BCB9F822", 16)
-// 	params.Gy, _ = new(big.Int).SetString("7DDE385D566332ECC0EABFA9CF7822FDF209F70024A57B1AA000C55B881F8111B2DCDE494A5F485E5BCA4BD88A2763AED1CA2B2FA8F0540678CD1E0F3AD80892", 16)
-// 	z, _ := new(big.Int).SetString("12EE58E6764838B69782136F0F2D3BA06E27695716054092E60A80BEDB212B64E585D90BCE13761F85C3F1D2A64E3BE8FEA2220F01EBA5EEB0F35DBD29D922AB", 16)
-// 	p512r1 = newrcurve(twisted, params, z)
-// }
-
-// // P256t1 returns a Curve which implements Brainpool P256t1 (see RFC 5639, section 3.4)
-// func P256t1() elliptic.Curve {
-// 	once.Do(initAll)
-// 	return p256t1
-// }
-
-// // P256r1 returns a Curve which implements Brainpool P256r1 (see RFC 5639, section 3.4)
-// func P256r1() elliptic.Curve {
-// 	once.Do(initAll)
-// 	return p256r1
-// }
-
-// // P384t1 returns a Curve which implements Brainpool P384t1 (see RFC 5639, section 3.6)
-// func P384t1() elliptic.Curve {
-// 	once.Do(initAll)
-// 	return p384t1
-// }
-
-// // P384r1 returns a Curve which implements Brainpool P384r1 (see RFC 5639, section 3.6)
-// func P384r1() elliptic.Curve {
-// 	once.Do(initAll)
-// 	return p384r1
-// }
-
-// // P512t1 returns a Curve which implements Brainpool P512t1 (see RFC 5639, section 3.7)
-// func P512t1() elliptic.Curve {
-// 	once.Do(initAll)
-// 	return p512t1
-// }
-
-// // P512r1 returns a Curve which implements Brainpool P512r1 (see RFC 5639, section 3.7)
-// func P512r1() elliptic.Curve {
-// 	once.Do(initAll)
-// 	return p512r1
-// }
